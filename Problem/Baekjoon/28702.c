872ad77e109cdba1ed81319865611f8d4b7f6267
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-06-16 21:03:10 +0900

    Update 28702.c

--- Problem/Baekjoon/28702.c
+++ Problem/Baekjoon/28702.c
@@ -18,15 +18,15 @@ int main()
 	char result[20];
 	char a[20],b[20],c[20];
 	scanf("%s %s %s",a,b,c);
-	
+
+	// 주어진 문자열들 중 숫자로 된 것이 있다면 해당 수에서 알맞은 수를 더하여 4번째에 배치될 수를 지정
 	if(a[0]>='0' && a[0]<='9')
-		FizzBuzz(atoi(a)+3);
+		FizzBuzz(atoi(a)+3); 
 	else if(b[0]>='0' && b[0]<='9')
 		FizzBuzz(atoi(b)+2);
 	else if(c[0]>='0' && c[0]<='9')
 		FizzBuzz(atoi(c)+1);
-	else
-		result[0]='\0';
 	
 	return 0;
 }
+// atoi는 주어진 문자열을 정수로 변환해주는 함수
