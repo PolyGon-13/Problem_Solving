3b061b8f65d6314af5b02bacd2c50b211bd87487
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2024-06-19 19:51:17 +0900

    Create 2669.c

---
+++ Problem/Baekjoon/2669.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int map[101][101]={0};
+
+int main()
+{
+	int a,b,c,d;
+	int sum=0;
+	
+	int i,j,k;
+	for(i=0;i<4;i++)
+	{
+		scanf("%d %d %d %d",&a,&b,&c,&d);
+		for(j=a;j<c;j++)
+		{
+			for(k=b;k<d;k++)
+				map[j][k]=1;
+		}
+	}
+	
+	for(i=0;i<101;i++)
+		for(j=0;j<101;j++)
+			if(map[i][j]==1)
+				sum++;
+	printf("%d\n",sum);
+	
+	return 0;
+}
