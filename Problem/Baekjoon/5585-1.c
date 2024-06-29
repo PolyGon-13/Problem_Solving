a54b802c5a047874ccf6665323a536c4f2959b65
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-06-29 19:35:39 +0900

    Update 5585-1.c

--- Problem/Baekjoon/5585-1.c
+++ Problem/Baekjoon/5585-1.c
@@ -1,6 +1,4 @@
-// 직접 쓴 코드이고 정답이지만 while문의 남발로 코드가 보기 좋지 않기에 5585-2.c에 다른 예시를 첨부함.
 #include <stdio.h>
-
 #define pay 1000
 
 int main()
@@ -52,3 +50,41 @@ int main()
 	
 	return 0;
 }
+
+// 위 코드는 직접 작성하였고 정답이지만 while문의 남발로 코드가 보기 좋지 않음
+// 기존에 작성했던 코드처럼 while문을 사용하고는 있지만 배열을 이용하여 효율적인 방식으로 작동하고 있음
+/*
+#include <stdio.h>
+
+void change()
+{
+	int coin[6]={500,100,50,10,5,1};
+	int cnt=0;
+	int n;
+	int i=0;
+	
+	scanf("%d",&n);
+	
+	n=1000-n;
+	
+	while(n>0)
+	{
+		if(n>=coin[i])
+		{
+			n-=coin[i];
+			cnt++;
+		}
+		else
+			i++;
+	}
+	
+	printf("%d\n",cnt);
+}
+
+int main()
+{
+	change();
+	
+	return 0;
+}
+*/
