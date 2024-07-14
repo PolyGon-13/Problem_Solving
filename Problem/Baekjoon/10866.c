79eac361cfb52cdc6d3af436094b71f14dd7a06c
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-14 22:55:02 +0900

    Update 10866.c

--- Problem/Baekjoon/10866.c
+++ Problem/Baekjoon/10866.c
@@ -32,11 +32,18 @@ int push_front(IntDeque *d,int x)
 	
 	d->num++;
 	if(--d->front<0)
-		d->front=d->max-1;
+		d->front=d->max-1; 
+	// front push는 배열의 시작 인덱스부터가 아닌 가장 뒤쪽 인덱스부터 push함
+	// max개의 요소를 갖는 배열을 만들었으므로 인덱스는 0부터 max-1까지 존재하기에 max-1이 마지막 인덱스임
 	d->deque[d->front]=x;
+	// 위쪽 if문에서 d->front를 검사할 때 참 거짓 여부 상관없이 --d->front로 인해 감소해서 오기 때문에 차곡차곡 데이터를 쌓을 수 있음
 	
 	return 0;
 }
+// 원형 큐를 직접 그려서 이해하면 쉬움
+// 0부터 6까지의 인덱스를 갖는 배열이 있다면, front는 6->5->4 순으로 데이터를 쌓고, rear는 0->1->2 순으로 데이터를 쌓음
+// front는 4까지, rear는 2까지 데이터를 쌓았다고 가정하고, 원형큐를 쭉 펼쳐서보면, [2 1 0 6 5 4] 처럼 나타낼 수 있음
+// 현재 rear는 2번 인덱스를, front는 4번 인덱스를 가리키고 있으므로 덱의 역할을 잘 수행하고 있다고 할 수 있음
 
 int push_back(IntDeque *d,int x)
 {
@@ -57,6 +64,8 @@ int pop_front(IntDeque *d,int *x)
 	
 	d->num--;
 	*x=d->deque[d->front++];
+	// front는 위의 주석의 예시를 그대로 가져오면 6->5->4 순으로 데이터를 쌓음
+	// 그러므로 4번 데이터를 pop하면 d->front를 1 증가시켜 5번 데이터를 가리키도록 하는 것이 맞음
 	d->front=d->front%d->max;
 	
 	return 0;
@@ -68,9 +77,13 @@ int pop_back(IntDeque *d,int *x)
 		return -1;
 	
 	d->num--;
-	if(--d->rear<0)
+	if(--d->rear<0) 
+	// push_back 함수에서 rear는 push 작업 수행 후 1 증가시켜 d->max를 넘는지 판단하는 코드가 존재했음
+	// 따라서 현재 아무 데이터가 없는 인덱스를 가리키고 있기에 1 감소시켜 가장 최신 데이터를 가리키도록 해줌
 		d->rear=d->max-1;
 	*x=d->deque[d->rear];
+	// 해당 작업 수행 후 d->rear를 수정해줄 필요는 없음
+	// 어짜피 push_back 함수에서 해당 데이터를 덮어 쓸 것이기에 d->num만 1 감소시켜주면 끝
 	
 	return 0;
 }
@@ -98,11 +111,10 @@ int back(const IntDeque *d)
 	if(d->num<=0)
 		return -1;
 	
-	int temp;
-	if((temp=d->rear-1)<0)
-		temp=d->max-1;
-	
-	return d->deque[temp];
+	if((d->rear-1)<0)
+		return d->deque[d->max-1];
+	else
+		return d->deque[d->rear-1];
 }
 
 int main()
