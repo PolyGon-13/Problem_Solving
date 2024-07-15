feb6fbf01cc74ec58c6ddcf626a4bff0979600a7
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-15 22:53:46 +0900

    Update 1158.c

--- Problem/Baekjoon/1158.c
+++ Problem/Baekjoon/1158.c
@@ -71,17 +71,20 @@ void Insert(List *plist,int data)
 	Node *newNode=(Node *)malloc(sizeof(Node));
 	newNode->data=data;
 	
-	if(plist->head==NULL)
+	if(plist->head==NULL) // 큐가 비어있는 상황
 	{
-		plist->head=newNode;
-		plist->tail=newNode;
-		newNode->next=newNode;
+		plist->head=newNode; // 머리노드가 신규노드를 가리키도록 함
+		plist->tail=newNode; // 꼬리노드가 신규노드를 가리키도록 함
+		newNode->next=newNode; // 신규노드의 다음노드는 자기 자신을 가리키도록 함
 	}
 	else
 	{
 		newNode->next=plist->tail->next;
+		// 신규노드가 이전의 꼬리노드가 다음노드로 가리키고 있었던 가장 처음 노드를 가리키도록 함
 		plist->tail->next=newNode;
+		// 이전 꼬리노드가 신규노드를 가리키도록 함
 		plist->tail=newNode;
+		// 꼬리노드를 신규노드로 수정
 	}
 }
 
@@ -94,32 +97,37 @@ void Cur_Init(List *plist)
 int Shift(List *plist,int k)
 {
 	plist->cur=plist->cur->next;
+	// Cur_Init에서 plist->cur이 꼬리노드를 가리키도록 설정되었는데, 꼬리노드의 다음 노드 즉, 머리노드를 가리키도록 함
 	
 	int i;
-	for(i=0;i<k-1;i++)
+	for(i=0;i<k-1;i++) // k번째로 이동해야 하므로, 0부터 k-1까지 반복
 	{
-		plist->before=plist->cur;
-		plist->cur=plist->cur->next;
+		plist->before=plist->cur; // 변경되기 전의 plist->cur의 값을 받아옴
+		plist->cur=plist->cur->next; // plist->cur은 머리노드부터 다음 노드로 계속 이동
 	}
 	
-	return plist->cur->data;
+	return plist->cur->data; // 제거해야할 데이터를 반환
 }
 
 void Remove(List *plist)
 {
-	Node *rpos=plist->cur;
-	if(rpos==plist->tail)
+	Node *rpos=plist->cur; // 제거해야 하는 노드를 지정
+	
+	if(rpos==plist->tail) // 꼬리노드를 제거하는 경우
 	{
-		if(plist->tail==plist->tail->next)
+		if(plist->tail==plist->tail->next) // 큐에 노드가 하나만 남은 경우 (이 문제에서는 노드를 더 이상 추가하지 않으므로 불필요한 작업)
 		{
 			plist->head=NULL;
 			plist->tail=NULL;
-		}
+		} 
 		else
-			plist->tail=plist->before;
+			plist->tail=plist->before; // 꼬리노드를 삭제하므로 꼬리노드를 그 이전의 노드로 수정
 	}
-	plist->before->next=plist->cur->next;
-	plist->cur=plist->before;
+	
+	// 꼬리 노드가 아닌 경우
+	plist->before->next=plist->cur->next; // 제거한 노드 이전의 노드와 제거한 노드 이후의 노드를 연결
+	plist->cur=plist->before; // list->cur을 제거한 노드 이전을 가리키도록 하여 다음 제거 작업을 준비
+	
 	free(rpos);
 }
 
