d7426011693ccd1a809fef0297441da3beb1c25e
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-07-15 22:41:35 +0900

    Update 1158.c

--- Problem/Baekjoon/1158.c
+++ Problem/Baekjoon/1158.c
@@ -18,7 +18,7 @@ int main()
 			
 			if(cnt==k) // k번째 사람을 찾은 경우
 			{
-				arr[j]=-1; // 해당 사람 제거
+				arr[j]=-1; // 해당 사람 제거 (배열의 모든 요소를 0으로 초기화한 뒤 사람을 제거했다면 -1로 바꾸는 방식)
 				if(i==n-1) // 마지막 사람을 제거하고 있는 경우
 					printf("%d>",j);
 				else
@@ -26,7 +26,7 @@ int main()
 				
 				break;
 			}
-			j++; // 다음 요소로 넘어가요
+			j++; // 다음 요소로 넘어감
 			if(j>n)
 				j=1;
 		}
@@ -36,4 +36,118 @@ int main()
 	return 0;
 }
 
-// 배열의 모든 요소를 0으로 초기화한 뒤 사람을 제거했다면 -1로 바꾸는 방식
+// 연결리스트를 이용한 풀이
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct __node
+{
+	int data;
+	struct __node *next;
+} Node;
+
+typedef struct _List
+{
+	Node *head; // 머리 노드
+	Node *tail; // 꼬리 노드
+	Node *cur; // 삭제할 노드의 위치
+	Node *before; // cur 바로 직전의 노드
+} CList;
+
+typedef CList List;
+
+void Init(List *plist)
+{
+	plist->head=NULL;
+	plist->tail=NULL;
+	plist->cur=NULL;
+	plist->before=NULL;
+}
+
+void Insert(List *plist,int data)
+{
+	Node *newNode=(Node *)malloc(sizeof(Node));
+	newNode->data=data;
+	
+	if(plist->head==NULL)
+	{
+		plist->head=newNode;
+		plist->tail=newNode;
+		newNode->next=newNode;
+	}
+	else
+	{
+		newNode->next=plist->tail->next;
+		plist->tail->next=newNode;
+		plist->tail=newNode;
+	}
+}
+
+void Cur_Init(List *plist)
+{
+	plist->cur=plist->tail;
+	plist->before=plist->tail;
+}
+
+int Shift(List *plist,int k)
+{
+	plist->cur=plist->cur->next;
+	
+	int i;
+	for(i=0;i<k-1;i++)
+	{
+		plist->before=plist->cur;
+		plist->cur=plist->cur->next;
+	}
+	
+	return plist->cur->data;
+}
+
+void Remove(List *plist)
+{
+	Node *rpos=plist->cur;
+	if(rpos==plist->tail)
+	{
+		if(plist->tail==plist->tail->next)
+		{
+			plist->head=NULL;
+			plist->tail=NULL;
+		}
+		else
+			plist->tail=plist->before;
+	}
+	plist->before->next=plist->cur->next;
+	plist->cur=plist->before;
+	free(rpos);
+}
+
+int main()
+{
+	int i;
+	int n,k;
+	scanf("%d %d",&n,&k);
+	
+	List list;
+	Init(&list);
+	
+	for(i=1;i<=n;i++)
+		Insert(&list,i);
+	
+	Cur_Init(&list);
+	
+	printf("<%d",Shift(&list,k));
+	Remove(&list);
+	
+	for(i=0;i<n-1;i++)
+	{
+		printf(", %d",Shift(&list,k));
+		Remove(&list);
+	}
+	putchar('>');
+	putchar('\n');
+	
+	return 0;
+}
+*/
