3208dc7534a37462793f2d62f108482971323318
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-08-08 15:48:50 +0900

    Create 15688.c

---
+++ Problem/Baekjoon/15688.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const int *a,const int *b)
+{
+	if(*a>*b)
+		return 1;
+	else if(*a<*b)
+		return -1;
+	else
+		return 0;
+}
+
+int main()
+{
+	int i;
+	int n;
+	scanf("%d",&n);
+	
+	int *x=calloc(n,sizeof(int));
+	for(i=0;i<n;i++)
+		scanf("%d",&x[i]);
+	
+	qsort(x,n,sizeof(x[0]),(int(*)(const void *,const void *))compare);
+	
+	for(i=0;i<n;i++)
+		printf("%d\n",x[i]);
+	
+	free(x);
+	
+	return 0;
+}
