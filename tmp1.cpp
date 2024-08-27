f24c677a5e2f747019ecb8922153ada0139e4d5a
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-08-27 19:19:57 +0900

    Create tmp1.cpp

---
+++ tmp1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n,a,b;
+	int x=0,y=0,z;
+	
+	cin>>n;
+	while(n--)
+	{
+		cin>>a>>b;
+		x+=a;
+		y+=b;
+		z=x-y;
+		cout<<z<<'\n';
+	}
+	
+	return 0;
+}
