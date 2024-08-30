6b3e74343b6b52652cff3836d2583a3b0b48b0cc
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-08-30 20:46:37 +0900

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int a,b,p,q;
+	
+	cin>>a>>b;
+	p=(a+1)/2;
+	q=(b+1)/2;
+	
+	cout<<q-p+1<<endl;
+	
+	return 0;
+}
+// 디미고에 가고 싶어.
