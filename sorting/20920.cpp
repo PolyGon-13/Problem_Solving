7906ca2c189b6517b2e69e192522553f9df5da19
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-08-31 09:40:29 +0900

    Update 20920.cpp

--- sorting/20920.cpp
+++ sorting/20920.cpp
@@ -2,6 +2,10 @@
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
+// unordered_map은 map보다 더 빠른 탐색을 하기 위한 자료구조로 해쉬테이블로 구현한 자료구조
+// unordered_map의 시간 복잡도는 O(1), map은 이진탐색으로 구현되어 O(logn)
+// unordered_map은 중복된 데이터를 허용하지 않고, map에 비해 데이터가 많을 때 좋은 성능을 보임
+// key가 유사한 데이터가 많으면 해시 충돌로 성능이 하락할 수 있음
 
 using namespace std;
 
