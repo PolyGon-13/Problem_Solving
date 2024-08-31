36f0d2b5e164e48ed8e9dea893ee4e1d604f2b02
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-08-31 16:01:22 +0900

    Update C1.cpp

--- C1.cpp
+++ C1.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int MOD = 1000000007;
+const int MOD=1000000007;
 
 int main()
 {
@@ -20,18 +20,18 @@ int main()
 		cin>>height[i];
 	
 	vector<int> dp(h+1,0);
-	dp[0]=1;
+	dp[0]=1; // 높이 0을 만드는 경우의 수
 	
 	for(int i=1;i<=h;i++)
 	{
 		for(int j:height)
 		{
-			if(i>=j)
+			if(i>=j) // i는 목표 높이, j는 컵의 높이이므로 i가 j보다 더 커야함
 				dp[i]=(dp[i]+dp[i-j])%MOD;
+				// 높이 i-1의 경우를 알고 있다면 해당 경우에 높이 1을 추가하여 높이 4의 경우의 수를 구할 수 있음
 		}
 	}
 	cout<<dp[h]<<'\n';
 	
 	return 0;
 }
-// 중앙대. 컵 쌓기
