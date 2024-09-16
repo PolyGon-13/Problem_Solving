8ea16329c643c1a8b317b9f739943774e7b73480
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-09-16 11:51:10 +0900

    Update 13301.cpp

--- dp/13301.cpp
+++ dp/13301.cpp
@@ -25,3 +25,32 @@ int main()
 	
 	return 0;
 }
+
+/*
+// 둘레를 dp 배열에 넣어 풀이
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n;
+	long long dp[81]={0,};
+	
+	cin>>n;
+	
+	dp[1]=4;
+	dp[2]=6;
+	
+	for(int i=3;i<=n;i++)
+		dp[i]=dp[i-1]+dp[i-2];
+	
+	cout<<dp[n]<<'\n';
+	
+	return 0;
+}
+*/
