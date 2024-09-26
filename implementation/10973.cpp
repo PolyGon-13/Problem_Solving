ce78aa31c234a40e4055be7e8de3f0de7568ac20
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-09-26 22:44:02 +0900

    Update 10973.cpp

--- implementation/10973.cpp
+++ implementation/10973.cpp
@@ -20,7 +20,7 @@ int main()
 	
 	for(int i=n-1;i>=1;i--)
 	{
-		if(arr[i]>arr[i+1])
+		if(arr[i]>arr[i+1]) // 뒤에서부터 수의 크기변화가 발생하는 부분을 탐색
 		{
 			idx=i;
 			break;
@@ -36,7 +36,7 @@ int main()
 	int tmp_idx=idx;
 	for(int i=idx+1;i<=n;i++)
 	{
-		if(arr[i]<arr[idx])
+		if(arr[i]<arr[idx]) // 탐색한 인덱스를 기준으로 해당 수보다 더 큰 수를 탐색
 		{
 			tmp=arr[i];
 			tmp_idx=i;
@@ -44,7 +44,7 @@ int main()
 	}
 	
 	swap(arr[idx],arr[tmp_idx]);
-	sort(arr+idx+1,arr+1+n,greater<>());
+	sort(arr+idx+1,arr+1+n,greater<>()); // 교환 지점 뒤에 숫자들 내림차순 정렬
 	
 	for(int i=1;i<=n;i++)
 		cout<<arr[i]<<" ";
