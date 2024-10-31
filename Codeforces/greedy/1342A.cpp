18a7453492818d212ba20ae652c08eca6b2a2fde
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-10-31 22:03:52 +0900

    Update 1342A.cpp

--- Codeforces/greedy/1342A.cpp
+++ Codeforces/greedy/1342A.cpp
@@ -3,38 +3,32 @@
 
 using namespace std;
 
+typedef long long ll;
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-	int t,x,y,a,b;
+	int t;
+	ll x,y,a,b;
 	
 	cin>>t;
 	while(t--)
 	{
+		ll ans1=0,ans2=0;
+
 		cin>>x>>y;
 		cin>>a>>b;
 		
-		int abs_x=abs(x);
-		int abs_y=abs(y);
-		int pay=0;
+		if(x<y)
+			swap(x,y);
 		
-		if(x*y>0)
-		{
-			int max_num=max(abs_x,abs_y);
-			int min_num=min(abs_x,abs_y);
-			
-			pay+=min_num*b;
-			pay+=(max_num-min_num)*a;
-		}
-		else
-		{
-			pay+=abs_x*a;
-			pay+=abs_y*b;
-		}
-		cout<<pay<<'\n';
+		ans1=x*a+y*a;
+		ans2=(y-x)*a+x*b;
+		
+		cout<<min(ans1,ans2)<<'\n';
 	}
 	
 	return 0;
